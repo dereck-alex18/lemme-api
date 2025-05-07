37c5ad1dbdc14c541cde0d844dd47367e3176831
Dereck Portela <38384320+dereck-alex18@users.noreply.github.com>
2025-05-07 15:19:13 +0000

    adiciona metodo get para retornar as configuracoes do site

--- src/site-config/site-config.controller.ts
+++ src/site-config/site-config.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { SiteConfigService } from './site-config.service';
 import { CreateSiteConfigDto } from './dto/create-site-config-dto';
 
 @Controller('site-config')
 export class SiteConfigController {
   constructor(private readonly siteConfigService: SiteConfigService) {}
+
+  @Get()
+  async getSiteConfig() {
+    return await this.siteConfigService.getSiteConfig();
+  }
 
   @Post()
   async postSiteConfig(@Body() dto: CreateSiteConfigDto) {
